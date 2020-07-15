3bcd739eeb57d3f1645f4c90883387961cb02363
yzh20020301 <67083551+yzh20020301@users.noreply.github.com>
2020-07-15 22:37:04 +0800

    Add files via upload

--- prophase/yzh/Icache1.v
+++ prophase/yzh/Icache1.v
@@ -608,8 +608,18 @@ begin
   if(rst)  begin  wdata=0;we=0;ins=32'b0;v[0]=0;v[1]=0;v[2]=0;v[3]=0;dir[0]=0;dir[1]=0;dir[2]=0;dir[3]=0; end
   case(s)
   FREE:we=0;
-  PD:  if(wreq&~miss) begin ins=din;wdx=1;ok=1;we=1;wed[linex]=wbyte;dir[lux][suoyin]=1; end
-       else if(~wreq&~miss) begin ins=cdat[lux][linex];ok=1;we=0; end
+  PD:  if(wreq&~miss) begin ins=din;wdx=1;ok=1;we=1;wed[linex]=wbyte;dir[lux][suoyin]=1; 
+             if(lru[0]<=lru[lux]) wel[0]=1;
+             if(lru[1]<=lru[lux]) wel[1]=1;
+             if(lru[2]<=lru[lux]) wel[2]=1;
+             if(lru[3]<=lru[lux]) wel[3]=1;
+             wel[lux]=1;lruc[lux]=1;end
+       else if(~wreq&~miss) begin ins=cdat[lux][linex];ok=1;we=0; 
+             if(lru[0]<=lru[lux]) wel[0]=1;
+             if(lru[1]<=lru[lux]) wel[1]=1;
+             if(lru[2]<=lru[lux]) wel[2]=1;
+             if(lru[3]<=lru[lux]) wel[3]=1;
+             wel[lux]=1;lruc[lux]=1;end
        else if(wreq&miss) we=1;
        else we=0;
   WB:  begin stall=1;sen=1;wen=1;
